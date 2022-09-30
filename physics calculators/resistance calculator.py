a5c152c395e509e08eb99284ac38036746219795
Era1305 <114339583+Era1305@users.noreply.github.com>
2022-09-30 17:28:47 +0300

    new calculator

---
+++ physics calculators/resistance calculator.py
@@ -0,0 +1,9 @@
+print("resistance calculator")
+print("Enter wire length")
+l=int(input())
+print("Enter p")
+p=int(input())
+print("Enter the cross-sectional area of the wire")
+S=int(input())
+R=(p*l)/S
+print(R)
